09522a9457717e61a9db86fa4261d1088fd0f6ca
Pavlo Zhukov <pahazhukov@gmail.com>
2019-01-22 03:20:32 +0200

    Add false positive unnecessary FQN example

---
+++ app/Enums/SomeOption.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class SomeOption extends Enum
+{
+    const OptionOne = 0;
+    const OptionTwo = 1;
+    const OptionThree = 2;
+}
--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function someHardcodedOption()
+    {
+        return \App\Enums\SomeOption::getValue(1); // wrong positive unnecessary FQN here
+    }
 }
